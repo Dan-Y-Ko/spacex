920ea6013f021020e5d4c22ccbf3448a4121e6e0
Dan <danko56666@gmail.com>
2020-07-04 16:22:56 -0400

    lecture 59

--- mod.ts
+++ mod.ts
@@ -1,10 +1,14 @@
-async function downloadLaunchData() {
-  const response = await fetch("https://api.spacexdata.com/v3/launches", {
-    method: "GET",
-  });
+const response = await fetch("https://reqres.in/api/users", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json; charset=UTF-8",
+  },
+  body: JSON.stringify({
+    name: "Elon Musk",
+    job: "billionaire",
+  }),
+});
 
-  const launchData = await response.json();
-  console.log(launchData);
-}
+const body = await response.json();
 
-await downloadLaunchData();
+console.log(body);
